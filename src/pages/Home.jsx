3128a3706e9da00bdd6392f228918e95c9c26447
rakha zufar <rakhazufar1@gmail.com>
2024-07-30 08:17:40 +0700

    edit homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Frontend</h1>
-      <h1 className="text-3xl font-bold">— Mockup</h1>
+    <div className="bg-gray-950 w-screen h-[90vh] flex justify-center items-center flex-col">
+      <div>
+        <h1 className="text-5xl font-bold text-white">Frontend</h1>
+        <h1 className="text-5xl font-bold text-white">— Mockup</h1>
+      </div>
     </div>
   );
 }
